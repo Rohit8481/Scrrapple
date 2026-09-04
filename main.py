c7a6aa96fc0886e7b81f47a70924bd2a21c75236
Rohit kumar <rk848199786@gmail.com>
2026-09-04 18:28:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,10 +7,11 @@ app = FastAPI()
 @app.get("/")
 def news():
   db = mysql.connector.connect(
-    host="gateway01.ap-southeast-1.prod.aws.tidbcloud.com",
-    user="2MeFkK3RJd7rXvv.root",
-    password="N2UuuYUcJmeZUsry",
-    database="newz_app"
+    host=os.getenv("TIDB_HOST"),
+    user=os.getenv("TIDB_USER"),
+    password=os.getenv("TIDB_PASSWORD"),
+    database=os.getenv("TIDB_DATABASE"),
+    port=int(os.getenv("TIDB_PORT", "4000"))
 )
 
   cur = db.cursor(dictionary=True)  # dictionary=True se clean JSON milta h
