2e8a07813e9f090565bca1a6567cdfb8ffabf5fe
Rohit kumar <rk848199786@gmail.com>
2026-09-04 18:27:19 +0530

    Update databse.py

--- databse.py
+++ databse.py
@@ -8,10 +8,11 @@ time = str(datetime.now().hour)
 #########################################
 
 db = mysql.connector.connect(
-    host="gateway01.ap-southeast-1.prod.aws.tidbcloud.com",
-    user="2MeFkK3RJd7rXvv.root",
-    password="N2UuuYUcJmeZUsry",
-    database="newz_app"
+    host=os.getenv("TIDB_HOST"),
+    user=os.getenv("TIDB_USER"),
+    password=os.getenv("TIDB_PASSWORD"),
+    database=os.getenv("TIDB_DATABASE"),
+    port=int(os.getenv("TIDB_PORT", "4000"))
 )
     
 cur = db.cursor()
