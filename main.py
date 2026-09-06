4b756d7b7885c6526cab545f4a47188b881e0405
Rohit kumar <rk848199786@gmail.com>
2026-09-06 12:18:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,33 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import mysql.connector
 import os
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
 def news():
-  db = mysql.connector.connect(
-    host=os.getenv("TIDB_HOST"),
-    user=os.getenv("TIDB_USER"),
-    password=os.getenv("TIDB_PASSWORD"),
-    database=os.getenv("TIDB_DATABASE"),
-    
-)
+    db = mysql.connector.connect(
+        host=os.getenv("TIDB_HOST"),
+        user=os.getenv("TIDB_USER"),
+        password=os.getenv("TIDB_PASSWORD"),
+        database=os.getenv("TIDB_DATABASE"),
+    )
 
-  cur = db.cursor(dictionary=True)  # dictionary=True se clean JSON milta h
-  cur.execute("SELECT * FROM news;")
-  results = cur.fetchall()
-  db.close()  
-  return results
+    cur = db.cursor(dictionary=True)
+    cur.execute("SELECT * FROM news;")
+    results = cur.fetchall()
+
+    cur.close()
+    db.close()
+
+    return results
