fd88f0e10c969d824012fde68589583777fdce82
Rohit kumar <rk848199786@gmail.com>
2026-09-07 22:07:33 +0530

    Update AllinOne.py

--- AllinOne.py
+++ AllinOne.py
@@ -158,7 +158,7 @@ def scrape(url):
 
         context = browser.new_context(
             user_agent=(
-                "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:125.0) "
+                "Mozilla/5.0 (X11; Linux x86_64; rv:125.0) " 
                 "Gecko/20100101 Firefox/125.0"
             ),
             viewport={
